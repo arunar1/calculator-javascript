9418d9efb4d967c75df3133c7c2c7cfbe4277dc3
Devadathan M B <devadathanmb@gmail.com>
2024-01-01 23:30:19 +0530

    format script.js

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-function ac()
-{
-    document.getElementById("res").value="";
+function ac() {
+  document.getElementById("res").value = "";
 }
-function show(input1)
-{
-    document.getElementById("res").value+=input1; 
+function show(input1) {
+  document.getElementById("res").value += input1;
 }
-function cal()
-{
-   var output=eval(document.getElementById("res").value);
-   document.getElementById("res").value=output;
+function cal() {
+  var output = eval(document.getElementById("res").value);
+  document.getElementById("res").value = output;
 }
